2f6b81a8cc4bd0ca11801b1f3e648bb93fe483c3
Abdelghani Moussaid <20841568+abdelghani-moussaid@users.noreply.github.com>
2024-07-22 17:27:21 +0100

    initial webpack commit

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development', 
+  devtool: 'inline-source-map',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html'
+    })
+  ]
+};
