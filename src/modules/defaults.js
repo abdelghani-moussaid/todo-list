24b83e969181ee7fbed8d169effb184f381f22da
Abdelghani Moussaid <20841568+abdelghani-moussaid@users.noreply.github.com>
2024-07-27 14:17:40 +0100

    clear default test data

--- src/modules/defaults.js
+++ src/modules/defaults.js
@@ -3,16 +3,14 @@ import Project from './project.js';
 import ProjectList from './projectList.js';
 
 export default function(){
-    const task = Todo("Feed cat", "Grain-Free Turkey", Date.now(), "high", false);
-    const task2 = Todo("Run", "Recovery run", Date.now(), "medium", false);
-    const task3 = Todo("JavaScript", "Project: The Weather App", Date.now(), "low", false);
+    // const task = Todo("Feed cat", "Grain-Free Turkey", Date.now(), "high", false);
+    // const task2 = Todo("Run", "Recovery run", Date.now(), "medium", false);
+    // const task3 = Todo("JavaScript", "Project: The Weather App", Date.now(), "low", false);
     const noProject = Project("Main");
-    const newProject = Project("The Odin Project");
-    noProject.addTodo(task);
-    noProject.addTodo(task2);
-    newProject.addTodo(task3)
+    // const newProject = Project("The Odin Project");
+    // newProject.addTodo(task3)
     const projectList = ProjectList();
     projectList.addProject(noProject);
-    projectList.addProject(newProject);
+    // projectList.addProject(newProject);
     return projectList;
 }
